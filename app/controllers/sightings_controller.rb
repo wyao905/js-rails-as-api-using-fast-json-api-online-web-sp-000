a7312fb10465d7615b28f17a538e682d5502b17f
Warren Yao <warrenyca905@gmail.com>
2020-01-23 02:42:09 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,10 @@
 class SightingsController < ApplicationController
+  
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings)
+  end
+
   def show
     @sighting = Sighting.find(params[:id])
     render json: SightingSerializer.new(sighting)
